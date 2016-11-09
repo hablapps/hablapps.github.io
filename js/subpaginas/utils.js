b73a57b96ee98f45a096f8fdc0bb313351b17abc
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2016-11-09 15:54:19 +0100

    new tags

--- js/subpaginas/utils.js
+++ js/subpaginas/utils.js
@@ -16,6 +16,8 @@ function loadPageFromFragment(){
 		case '#functional-101-ed2-es':
 		case '#functional-101-ed3':
 		case '#functional-101-ed3-es':
+		case '#functional-101-ed4':
+		case '#functional-101-ed4-es':
 		case '#functional-advanced':
 		case '#functional-advanced-es':
 		case '#functional-advanced-ed1':
